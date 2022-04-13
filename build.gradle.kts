88935aa7cc7d012bb4e50ccdab6d98ca1514fa89
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-04-13 10:35:10 +0200

    update commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20-RC"
+    kotlin("jvm") version "1.6.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("io.papermc.paperweight.userdev") version "1.3.5"
     id("xyz.jpenilla.run-paper") version "1.0.6"
