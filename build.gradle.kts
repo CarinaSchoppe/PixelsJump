d79727625742cfbe0a95179ba6e18057359032f7
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-06-13 12:46:23 +0200

    update commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ java {
 }
 tasks {
     runServer {
-        minecraftVersion("1.18.2")
+        minecraftVersion("1.19")
     }
     compileJava {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
